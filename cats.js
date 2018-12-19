203343641ce2e6e362db7e000ef161c67ec4ec39
NegaAshley <ashleymxu@gmail.com>
2018-12-19 04:53:26 +0000

    Add a cat to the DB

--- cats.js
+++ cats.js
@@ -7,6 +7,27 @@ var catSchema = new mongoose.Schema({
     temperament: String
 });
 
+var Cat = mongoose.model("Cat", catSchema);
+
+Cat.create
+
 //Add a new cat to the DB
+
+var meepo = new Cat({
+    name: "Meepo",
+    age: 5,
+    temperament: "curious"
+});
+
+//Save cat to DB and have callback function once that is done
+
+meepo.save(function(err, cat){
+    if(err){
+        console.log("Something went wrong!");
+    }else{
+        console.log("We just saved a cat to the DB!");
+        console.log(cat);
+    }
+});
 
 //Retrieve all cats from the DB and console.log each one
